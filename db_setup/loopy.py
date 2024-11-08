7e60d12bb1d97b97b769f91c4051f66194d0710a
j86park <156730513+j86park@users.noreply.github.com>
2024-11-08 14:13:58 -0500

    update

--- db_setup/loopy.py
+++ db_setup/loopy.py
@@ -1,83 +1,130 @@
 from init import connect_to_db
 from dotenv import load_dotenv
-import os
 import numpy as np # Import for numpy array
 import gpxpy # Import to allow easier parsing of gpx file
 import math
-import srtm #Import to query elevation data using lon and lat data
-import psycopg2
+from routemodel import init_table
+from routemodel import insert_data
 
 load_dotenv()
 
-# Create an instance of the SRTM elevation data
-elevation_data = srtm.get_data()
-
-def gpx_loop_parser(output_file="output.txt"):
+def gpx_parser():
     """
-    Convert gpx files into an array with
+    Parse GPX loopy files to extract essential route data and save it to a structured array
     
-    - lat
-    - long
-    - elevation (m)
-    - distance (km)
-    - orientation (deg)
+    - Stage names
+    - Latitudes
+    - Longitudes
+    - Elevations (m)
+    - Distances (km)
+    - Orientations (deg)
+    - Road angles (deg)
 
     As the respective columns.
     """
+    
+    # Initialize lists for each data component
+    data, stage_names, lats, lons, elevations = [], [], [], [], []
 
-    # Declare lists to hold data
-    data, lats, lons, elevations = [], [], [], []
-
-    # Open gpx file and utilize gpxpy to parse the file 
-    with open("db_setup/gpx_asc24//1AL_PaducahLoop.gpx", "r") as gpx_file:
-        # gpxpy stores the data in a much easier readable format
+    # Open gpx file and parse the file
+    with open("gpx_asc24/0_FullBaseRoute.gpx", "r") as gpx_file:
         gpx = gpxpy.parse(gpx_file)
 
-    # Since there's only one track, access it directly
-    track = gpx.tracks[0]
-
     # Iterate through all track segments and points
-    for segment in track.segments:
-        for point in segment.points:
-            lat = point.latitude
-            lon = point.longitude
-            lats.append(lat)
-            lons.append(lon)
-
-            # Get elevation using SRTM and append to the elevations list
-            elevation = get_srtm_elevation(lat, lon)
-            elevations.append(elevation) 
-
-    # Stores distance and orientation from returned data
-    distance = distance_calc(lats, lons)
-    orientation = orientation_calc(lats, lons)
-
+    for track in gpx.tracks:
+        for segment in track.segments:
+            for points in segment.points:
+                stage_names.append(track.name)
+                lats.append(points.latitude)
+                lons.append(points.longitude)
+                elevations.append(points.elevation)
+    
+    # Calculate distances, orientations, and road angles
+    distances = distance_calc(lats, lons).tolist()
+    orientations = orientation_calc(lats, lons).tolist()
+    road_angles = gradient_calculator(lats, lons, elevations, window_size=3).tolist()
+    
+    # Zip the individual arrays together 
+    # Specify a structured dtype (data type) for each column
+    # Need to add distance and orientation after
     # Create a structured numpy array for easier handling of the data
     data = np.array(list(zip(
+        stage_names,
         lats,
         lons,
         elevations,
-        distance,
-        orientation
+        distances,
+        orientations,
+        road_angles
     )), dtype=[
+        ("stage_name", object),
         ("lat", object),
         ("lon", object),
         ("ele", object),
         ("distance", object),
         ("orientation", object),
+        ("road_angle", object),
     ])
 
     print("Data successfully extracted to structured array")
     return data
 
-def get_srtm_elevation(lat, lon):
+def init_table():
     """
-    Fetch the elevation for a given latitude and longitude using SRTM library.
+    Initializes a table for route data in a PostgreSQL database. 
+    It ensures that the table doesn't already exist before creating it.
     """
-    elevation = elevation_data.get_elevation(lat, lon)
+    connection = connect_to_db()
+    cursor = connection.cursor()
+    connection.autocommit = True
 
-    return elevation if elevation is not None else 0
+    try:
+        cursor.execute(
+            """
+            CREATE TABLE route_model (
+                stage_name       VARCHAR(255),
+                lat              FLOAT,
+                long             FLOAT,
+                elevation        FLOAT,
+                distance         FLOAT,
+                orientation      FLOAT,
+                road_angle       FLOAT
+            );
+            """
+        )
+        print("Table route_model successfully created")
+    except Exception as e:
+        print(f"Error: Could not create route_model table: {e}")
+    finally:
+        cursor.close()
+        connection.close()
+    
+    return None
 
+def insert_data():
+    """
+    Insert data into route_model table in postgres database
+    """
+    connection = connect_to_db()
+    cursor = connection.cursor()
+    connection.autocommit = True
+
+    try:
+        insert_query = """
+            INSERT INTO route_model (stage_name, lat, long, elevation, distance, orientation, road_angle)
+            VALUES (%s, %s, %s, %s, %s, %s, %s)
+        """
+        cursor.executemany(insert_query, gpx_parser())
+        print("Data successfully inserted into route_model table")
+    except Exception as e:
+        print(
+            f"Error: Could not insert data into route_model table in postgres database: {e}"
+        )
+    finally:
+        cursor.close()
+        connection.close()
+
+    return None
 
 def euclidean_distance(lat1, lon1, lat2, lon2):
     """
@@ -113,6 +160,7 @@ def distance_calc(lats, lons):
     print("Cumulative distances successfully calculated")
     return distances
 
+
 def orientation_calc(lats, lons):
     """
     Calculate the orientations of the car (bearing) with True North being 0 degrees.
@@ -136,5 +184,58 @@ def orientation_calc(lats, lons):
     print("Orientations successfully calculated")
     return orientations
 
-data = gpx_loop_parser("gpx_output.txt")
-print(data)
+def moving_median(data, window_size):
+    """
+    Applies a moving median to the data.
+    Pads the data to handle edge cases and calculates the median for each window.
+    Outputs an array without the added padding.
+    """
+
+    medians = np.zeros(len(data))
+
+    # half window size is used as our width to ensure that the window is centered.
+    half_window = window_size // 2
+    padded_data = np.pad(data, (half_window, half_window), mode="edge")
+
+    for i in range(half_window, len(padded_data) - half_window):
+        window = padded_data[i - half_window : i + half_window]
+        medians[i - half_window] = np.median(window)
+    
+    print("Moving median successfully calculated")
+    return medians
+
+def gradient_calculator(lats, lons, elevations, window_size):
+    """
+    Calculates the angle of the road at each point in the route.
+    Applies a moving average to smooth the elvation data.
+    """
+
+    angles = np.zeros(len(lats) - 1)
+
+    for i in range(len(lats) - 1):
+        if i == len(lats) - 2:
+            # Calculate the horizontal distance using previous point
+            distance = euclidean_distance(lats[i - 1], lons[i - 1], lats[i], lons[i])
+            # Calculate the elevation difference using previous point
+            elevation_diff = elevations[i] - elevations[i - 1]
+            # Calculate the angle in radians and then convert to degrees
+            angle = math.degrees(math.atan2(elevation_diff, distance))
+            angles[i] = angle
+
+        else:
+            # Calculate the horizontal using next point
+            distance = euclidean_distance(lats[i], lons[i], lats[i + 1], lons[i + 1])
+            # Calculate the elevation difference using next point
+            elevation_diff = elevations[i + 1] - elevations[i]
+            # Calculate the angle in radians and then convert to degrees
+            angle = math.degrees(math.atan2(elevation_diff, distance))
+            angles[i] = angle
+
+    smoothed_angles = moving_median(angles, window_size)
+
+    print("Road angles successfully calculated")
+    return smoothed_angles
+
+if __name__ == "__main__":
+    init_table()
+    insert_data()
--- db_setup/routemodel.py
+++ db_setup/routemodel.py
@@ -1,40 +1,30 @@
-from init import connect_to_db
-from dotenv import load_dotenv
-import os
-import numpy as np # Import for numpy array
-import gpxpy # Import to allow easier parsing of gpx file
+import numpy as np
+import gpxpy
 import math
+from init import connect_to_db
 
-
-load_dotenv()
-
-def gpx_parser(output_file="output.txt"):
+def gpx_parser():
     """
-    Convert gpx files into an array with
+    Parse GPX files to extract essential route data and save it to a structured array.
     
-    - stage name
-    - lat
-    - long
-    - elevation (m)
-    - distance (km)
-    - orientation (deg)
-
-    As the respective columns.
+    Data includes:
+    - Stage names
+    - Latitudes
+    - Longitudes
+    - Elevations (m)
+    - Distances (km)
+    - Orientations (deg)
+    - Road angles (deg)
     """
-    
-    # Declare lists to hold data
-    data = []
-    stage_names = []
-    lats = []
-    lons = []
-    elevations = []
 
-    # Open gpx file and utilize gpxpy to parse the file
-    with open("gpx_asc24/0_FullBaseRoute.gpx", "r") as gpx_file:
-        # gpxpy stores the data in a much easier readable format
+    # Initialize lists for each data component
+    data, stage_names, lats, lons, elevations = [], [], [], [], []
+
+    # Load and parse the GPX file
+    with open("db_setup/gpx_asc24/0_FullBaseRoute.gpx", "r") as gpx_file:
         gpx = gpxpy.parse(gpx_file)
 
-    # Iterate through all track segments and points
+    # Extract data from the GPX structure
     for track in gpx.tracks:
         for segment in track.segments:
             for points in segment.points:
@@ -42,106 +32,47 @@ def gpx_parser(output_file="output.txt"):
                 lats.append(points.latitude)
                 lons.append(points.longitude)
                 elevations.append(points.elevation)
-    
-    # Stores distance and orientation from returned data
-    distance = distance_calc()
-    orientation = orientation_calc(lats, lons)
-    
-    # Convert lists to numpy arrays
-    np_stage_names = np.array(stage_names)
-    np_lats = np.array(lats)
-    np_lons = np.array(lons)
-    np_elevations = np.array(elevations)
-    np_distance = np.array(distance) # Assuming returend data was list
-    np_orientation = np.array(orientation) # Assuming returend data was list
-    
-    # Zip the individual arrays together 
-    # Specify a structured dtype (data type) for each column
-    # Need to add distance and orientation after
-    data = np.array(list(zip(np_stage_names, np_lats, np_lons, np_elevations)), dtype=[
-            ('stage_name', 'U50'), 
-            ('lat', 'f8'), 
-            ('lon', 'f8'), 
-            ('ele', 'f8') 
-        ])
 
-    # To test
-    np.savetxt(output_file, data, fmt="%s,%.8f,%.8f,%.2f", header="Stage Name, Latitude, Longitude, Elevation", delimiter=',', comments='')
-    
+    # Calculate distances, orientations, and road angles
+    distances = distance_calc(lats, lons).tolist()
+    orientations = orientation_calc(lats, lons).tolist()
+    road_angles = gradient_calculator(lats, lons, elevations, window_size=3).tolist()
+
+    # Create a structured numpy array for easier handling of the data
+    data = np.array(list(zip(
+        stage_names,
+        lats,
+        lons,
+        elevations,
+        distances,
+        orientations,
+        road_angles
+    )), dtype=[
+        ("stage_name", object),
+        ("lat", object),
+        ("lon", object),
+        ("ele", object),
+        ("distance", object),
+        ("orientation", object),
+        ("road_angle", object),
+    ])
+
+    print("Data successfully extracted to structured array")
     return data
-
-
-def distance_calc():
-    """
-    Calculate cumulative distances of a stage at each point.
-    """
-    # checking longitude and latitude array lengths
-    if len(latitudes) != len(longitudes):
-        raise ValueError("Issue in arrays.")
-    
-    # initializing cumulative distance array
-    distances = []
-    sum_distance = 0
-
-    # applying euclidean distances method
-    for i in range(len(longitudes) - 1):
-        current_distance = np.sqrt(((latitudes[i + 1] - latitudes[i])**2) + ((longitudes[i + 1] - longitudes[i])**2))
-        sum_distance = sum_distance + current_distance
-        distances.append(sum_distance)
-
-    return distances
-
-
-def orientation_calc(lats, lons):
-    """
-    Calculate the orientations of the car (bearing) with True North being 0 degrees.
-    Using this formula for bearing: 
-    https://www.movable-type.co.uk/scripts/latlong.html#:~:text=a%20constant%20bearing!-,Bearing,-In%20general%2C%20your
-    """
-    orientations = []
-    for i in range(len(lats)-1):
-        lat1 = lats[i]
-        lon1 = lons[i]
-        lat2 = lats[i+1]
-        lon2 = lons[i+1]
-        
-        # math functions need values in radians
-        rad_x1 = math.radians(lon1)
-        rad_x2 = math.radians(lon2)
-        rad_y1 = math.radians(lat1)
-        rad_y2 = math.radians(lat2)
-        diff_lon = rad_x2 - rad_x1
-        
-        # using spherical geometry for this formula
-        distance_y = math.sin(diff_lon) * math.cos(rad_y2)
-        distance_x = math.cos(rad_y1) * math.sin(rad_y2) - \
-            math.sin(rad_y1) * math.cos(rad_y2) * math.cos(diff_lon)
-        angle = math.atan2(distance_y, distance_x)
-        # convert angle to bearing in degrees
-        bearing = (angle*180/math.pi + 360) % 360
-        
-        orientations.append(bearing)
-    return orientations
 
 
 def init_table():
     """
-    Creates six-column unindexed table for route_model in postgres database.
-    Presumes the postgres database has already been created.
-    If table already exists, logs an error and does nothing.
+    Initializes a table for route data in a PostgreSQL database. 
+    It ensures that the table doesn't already exist before creating it.
     """
-    connection = connect_to_db(
-        db_user=os.getenv("DB_USER"),
-        db_host=os.getenv("HOST"),
-        db_name=os.getenv("DB_NAME"),
-        db_password=os.getenv("DB_PASSWORD")
-    )
-
-    connection.autocommit = True
+    connection = connect_to_db()
     cursor = connection.cursor()
+    connection.autocommit = True
 
     try:
-        cursor.execute("""
+        cursor.execute(
+            """
             CREATE TABLE route_model (
                 stage_name       VARCHAR(255),
                 lat              FLOAT,
@@ -149,37 +80,39 @@ def init_table():
                 elevation        FLOAT,
                 distance         FLOAT,
                 orientation      FLOAT,
+                road_angle       FLOAT
             );
-        """)
+            """
+        )
+        print("Table route_model successfully created")
     except Exception as e:
-        print(f"Error: Could not create route_model table in postgres database: {e}")
+        print(f"Error: Could not create route_model table: {e}")
     finally:
         cursor.close()
         connection.close()
-
+    
     return None
 
 
-def insert_data(txt_filepath):
+def insert_data():
     """
     Insert data into route_model table in postgres database
     """
-    connection = connect_to_db(
-        db_user=os.getenv("DB_USER"),
-        db_host=os.getenv("HOST"),
-        db_name=os.getenv("DB_NAME"),
-        db_password=os.getenv("DB_PASSWORD")
-    )
-    
-    connection.autocommit = True
+    connection = connect_to_db()
     cursor = connection.cursor()
+    connection.autocommit = True
 
     try:
-        with open(txt_filepath, 'r') as f:
-            next(f)  # skip header
-            cursor.copy_from(f, 'route_model', sep=',')
+        insert_query = """
+            INSERT INTO route_model (stage_name, lat, long, elevation, distance, orientation, road_angle)
+            VALUES (%s, %s, %s, %s, %s, %s, %s)
+        """
+        cursor.executemany(insert_query, gpx_parser())
+        print("Data successfully inserted into route_model table")
     except Exception as e:
-        print(f"Error: Could not insert data into route_model table in postgres database: {e}")
+        print(
+            f"Error: Could not insert data into route_model table in postgres database: {e}"
+        )
     finally:
         cursor.close()
         connection.close()
@@ -187,5 +120,119 @@ def insert_data(txt_filepath):
     return None
 
 
-gpx_parser("gpx_output.txt")
-insert_data("gpx_output.txt")
+def euclidean_distance(lat1, lon1, lat2, lon2):
+    """
+    Computes the Euclidean distance between two geographical points.
+    """
+    R = 6371000  # Earth's radius in meters
+
+    # Convert degrees to radians
+    lat1, lon1, lat2, lon2 = map(math.radians, [lat1, lon1, lat2, lon2])
+
+    # Convert spherical to Cartesian coordinates
+    x1, y1, z1 = R * math.cos(lat1) * math.cos(lon1), R * math.cos(lat1) * math.sin(lon1), R * math.sin(lat1)
+    x2, y2, z2 = R * math.cos(lat2) * math.cos(lon2), R * math.cos(lat2) * math.sin(lon2), R * math.sin(lat2)
+
+    # Calculate the Euclidean distance
+    return math.sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2)
+
+
+def distance_calc(lats, lons):
+    """
+    Computes cumulative distances for a series of latitude and longitude points.
+    """
+    if len(lats) != len(lons):
+        raise ValueError("Latitude and longitude arrays must be the same length.")
+
+    distances = np.zeros(len(lats))
+    sum_distance = 0
+
+    for i in range(1, len(lons)):
+        current_distance = euclidean_distance(lats[i - 1], lons[i - 1], lats[i], lons[i])
+        sum_distance += current_distance
+        distances[i] = sum_distance
+
+    print("Cumulative distances successfully calculated")
+    return distances
+
+
+def orientation_calc(lats, lons):
+    """
+    Calculate the orientations of the car (bearing) with True North being 0 degrees.
+    Using this formula for bearing:
+    https://www.movable-type.co.uk/scripts/latlong.html#:~:text=a%20constant%20bearing!-,Bearing,-In%20general%2C%20your
+    """
+    orientations = np.zeros(len(lats) - 1)
+    for i in range(len(lats) - 1):
+        lat1, lon1 = map(math.radians, (lats[i], lons[i]))
+        lat2, lon2 = map(math.radians, (lats[i + 1], lons[i + 1]))
+
+        # Spherical geometry to calculate bearing
+        diff_lon = lon2 - lon1
+        distance_y = math.sin(diff_lon) * math.cos(lat2)
+        distance_x = math.cos(lat1) * math.sin(lat2) - math.sin(lat1) * math.cos(lat2) * math.cos(diff_lon)
+        angle = math.atan2(distance_y, distance_x)
+        bearing = (math.degrees(angle) + 360) % 360
+
+        orientations[i] = bearing
+
+    print("Orientations successfully calculated")
+    return orientations
+
+
+def moving_median(data, window_size):
+    """
+    Applies a moving median to the data.
+    Pads the data to handle edge cases and calculates the median for each window.
+    Outputs an array without the added padding.
+    """
+
+    medians = np.zeros(len(data))
+
+    # half window size is used as our width to ensure that the window is centered.
+    half_window = window_size // 2
+    padded_data = np.pad(data, (half_window, half_window), mode="edge")
+
+    for i in range(half_window, len(padded_data) - half_window):
+        window = padded_data[i - half_window : i + half_window]
+        medians[i - half_window] = np.median(window)
+    
+    print("Moving median successfully calculated")
+    return medians
+
+
+def gradient_calculator(lats, lons, elevations, window_size):
+    """
+    Calculates the angle of the road at each point in the route.
+    Applies a moving average to smooth the elvation data.
+    """
+
+    angles = np.zeros(len(lats) - 1)
+
+    for i in range(len(lats) - 1):
+        if i == len(lats) - 2:
+            # Calculate the horizontal distance using previous point
+            distance = euclidean_distance(lats[i - 1], lons[i - 1], lats[i], lons[i])
+            # Calculate the elevation difference using previous point
+            elevation_diff = elevations[i] - elevations[i - 1]
+            # Calculate the angle in radians and then convert to degrees
+            angle = math.degrees(math.atan2(elevation_diff, distance))
+            angles[i] = angle
+
+        else:
+            # Calculate the horizontal using next point
+            distance = euclidean_distance(lats[i], lons[i], lats[i + 1], lons[i + 1])
+            # Calculate the elevation difference using next point
+            elevation_diff = elevations[i + 1] - elevations[i]
+            # Calculate the angle in radians and then convert to degrees
+            angle = math.degrees(math.atan2(elevation_diff, distance))
+            angles[i] = angle
+
+    smoothed_angles = moving_median(angles, window_size)
+
+    print("Road angles successfully calculated")
+    return smoothed_angles
+
+if __name__ == "__main__":
+    init_table()
+    insert_data()
