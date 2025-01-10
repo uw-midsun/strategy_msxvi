3b86b71a7be4828836fb2712cfee9d8e7c2e7aac
EricGaoo <mail.ericgao@gmail.com>
2025-01-10 00:10:17 -0500

    More info on DB keys

--- telemetry/db_upload.py
+++ telemetry/db_upload.py
@@ -15,28 +15,77 @@ write_api = client.write_api(write_options=SYNCHRONOUS)
 '''
 Telemetry Data:
 - battery_status
+    - fault
+    - fault value
+    - aux_battery_v
+    - afe_status
 - battery_vt
+    - voltage
+    - current
+    - temperature
+    - batt_perc
 - AFE 1
+    - id
+    - temp
+    - v1
+    - v2
+    - v3
 - AFE 2
+    - id
+    - temp
+    - v1
+    - v2
+    - v3
 - AFE 3
+    - id
+    - temp
+    - v1
+    - v2
+    - v3
 - cc_info
+    - target velocity
+    - drive state
+    - cruise_control
+    - regen_breaking
+    - hazard_enabled
 - cc_pedal
+    - throttle_output
+    - brake_output
 - cc_steering
-- Motor_controller_vc 
+    - input_cc
+    - input_lights
+- Motor_controller_vc
+    - mc_voltage_l
+    - mc_current_l
+    - mc_voltage_r
+    - mc_current_r
 - motor_velocity
+    - velocity_l
+    - velocity_r
+    - brakes_enabled
 - mc_status
+    - limit_bitset_l
+    - error_bitset_l
+    - limit_bitset_r
+    - error_bitset_r
+    - board_fault_bitset
+    - overtemp_bitset
+    - precharge_status
 - pd_status
+    - power_state
+    - fault_bitset
+    - bps_persist
+    - bps_persist_val
 '''
 
 while True:
     #example:
-    temperature = random.uniform(20.0, 25.0)
-    current = random.uniform(5.0, 15.0)
+
 
     # Create data point
     point = Point("telemetry") \
-        .field("temperature", temperature) \
-        .field("current", current) \
+        .field("key", value) \
+        .field("key", value) \
         .time(time.time_ns())
 
     write_api.write(bucket=bucket, org=org, record=point)
