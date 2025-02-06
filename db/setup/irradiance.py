c8aa669acfa151ffbc44ebe3fe801999bed78b87
Song Eric Yu Li <ericli3690@gmail.com>
2025-02-06 13:12:25 -0500

    wrote get_irradiance created three ways for the script to generate/retrieve irradiance and weather data: solcast forecast, solcast historical, and simulation

--- db/setup/irradiance.py
+++ db/setup/irradiance.py
@@ -1,21 +1,128 @@
-from db.connect import connect_to_db
 import os
+import datetime
+import pytz
+import requests
+import random
+import math
+from dotenv import load_dotenv
 
 
-def get_irradiance(lat, long, API_KEY):
+# need to import connect_to_db from db/connect.py
+import sys
+sys.path.append("db")
+from connect import connect_to_db
+
+
+def get_irradiance(API_KEY, lat, lon, azimuth, tilt, hours,
+                   is_getting_fake_data=False, use_historical_as_fake_instead_of_simulated=False, start_time=datetime.datetime(2024,8,15,9,30,0, tzinfo=pytz.FixedOffset(-5 * 50))):
     """
-    Pull 72 hrs data from solcast api as a csv
-    - air temp
-    - global tilted irradiance
-    - precipitation_rate
-    - relative humidity
-    - wind_direction_10m
-    - wind_speed_10m
+    Retrieves irradiance data at a lat/lon point as an array from earliest entry to latest entry
+    With each entry in the array as an object containing the following fields:
+    - air_temp (degC)
+    - gti (tilted irradiance, W/m^2)
+    - precipitation_rate (we only retrieve rain data as snow should not be factor in the summer, mm/h)
+    - wind_speed_10m (wind speed at 10m elevation, m/s)
+    - wind_direction_10m (wind direction at 10m elevation, degrees from 0 to 360)
+    Has three modes:
+    - real: queries the Solcast forecast API
+    - fake-historical: queries the Solcast historical API
+    - fake-simulated: generates a facsimile of the above output data using mathematical functions
+    Requirements:
+    - if start_time is provided, it must be a datetime object with timezone information specified
+        via dt = dt.replace(tzinfo=pytz.FixedOffset(-h * 60)); the start_time default parameter is
+        an arbitrary East Coast summer date in 2024 that has this already specified
+    - note start_time will likely only function properly in the Western hemisphere
     """
 
-    url = f"https://api.solcast.com.au/data/live/radiation_and_weather?latitude={lat}&longitude={long}&hours=72&output_parameters=air_temp%2Cgti%2Cprecipitation_rate%2Crelative_humidity%2Cwind_direction_10m%2Cwind_speed_10m&array_type=fixed&terrain_shading=true&format=csv&api_key={API_KEY}"
+    URL = ""
+    if is_getting_fake_data:
 
-    return None
+        end_time = start_time + datetime.timedelta(hours=hours)
+
+        if use_historical_as_fake_instead_of_simulated:
+
+            # Use historical data as a facsimile of forecast data
+            if not start_time.utcoffset():
+                raise Exception("Error: When fetching fake irradiance data, the start time datetime specified must have timezone information encoded.")
+            timezone_hour_offset = start_time.utcoffset().seconds//3600 - 24 # note this will break in places other than the western hemisphere
+            URL = f"https://api.solcast.com.au/data/historic/radiation_and_weather?api_key={API_KEY}" \
+                f"&latitude={lat}&longitude={lon}&azimuth={azimuth}&tilt={tilt}&array_type=fixed" \
+                f"&start={start_time.isoformat()}&end={end_time.isoformat()}&period=PT30M&format=json&time_zone={timezone_hour_offset}" \
+                f"&output_parameters=air_temp,gti,precipitation_rate,wind_speed_10m,wind_direction_10m"
+            
+        else:
+
+            # Simulate a facsimile of solcast data for testing purposes
+            output_array = []
+            curr_time = start_time
+
+            def air_temp_sim_at_hour(h):
+                """
+                Returns a simulation of temperature in degrees Celsius using a sinusoidal graph
+                Includes some randomization, is based on hour, outputs to two decimal places
+                Requires a float hour in the range 0.0 to 24.0, inclusive
+                """
+                return round(-8 * math.cos(math.pi / 12 * (h - 2)) + 14 + random.uniform(-2, 2), 2)
+            
+            def gti_sim_at_hour(h):
+                """
+                Returns a simulation of gti in W/m^2 using a parabolic graph
+                Includes some randomization, is based on hour, outputs to two decimal places
+                Requires a float hour in the range 0.0 to 24.0, inclusive
+                """
+                return max(round(-13 * (h-8) * (h-20) + random.uniform(0, 100), 2), 0)
+                
+            def precipitation_rate_sim_at_hour(h):
+                """
+                Returns a simulation of a little bit of afternoon rain in mm/h using a parabolic graph
+                Includes some randomization, is based on hour, outputs to two decimal places
+                Requires a float hour in the range 0.0 to 24.0, inclusive
+                """
+                return max(round(-5 * (h-15) * (h-17) + random.uniform(-1,1), 2), 0)
+            
+            def wind_speed_10m_sim_at_hour(h):
+                """
+                Returns a simulation of wind speed in m/s using a sinusoidal graph
+                Includes some randomization, is based on hour, outputs to two decimal places
+                Requires a float hour in the range 0.0 to 24.0, inclusive
+                """
+                return max(round(4 * math.sin(h) + 4 + random.uniform(-1, 1), 2), 0)
+            
+            def wind_direction_10m_sim_at_hour(h):
+                """
+                Returns a simulation of wind direction, in degrees from 0 to 360, using a sinusoidal graph
+                Includes some randomization, is based on hour, outputs to two decimal places
+                Requires a float hour in the range 0.0 to 24.0, inclusive
+                """
+                return round(180 * math.sin(h/random.uniform(48,52))+180,2)
+
+            while curr_time < end_time:
+                curr_hour_float = curr_time.hour + curr_time.minute/60
+                output_array.append({
+                    "air_temp": air_temp_sim_at_hour(curr_hour_float),
+                    "gti": gti_sim_at_hour(curr_hour_float),
+                    "precipitation_rate": precipitation_rate_sim_at_hour(curr_hour_float),
+                    "wind_speed_10m": wind_speed_10m_sim_at_hour(curr_hour_float),
+                    "wind_direction_10m": wind_direction_10m_sim_at_hour(curr_hour_float),
+                    # We follow the solcast data return format, which also always includes the following:
+                    "period_end": curr_time.isoformat(),
+                    "period": "PT30M"
+
+                })
+                curr_time += datetime.timedelta(minutes=30) # 0.5 hour discretization
+            return output_array
+
+    else:
+
+        # Actually call the forecast API
+        URL = f"https://api.solcast.com.au/data/live/radiation_and_weather?api_key={API_KEY}" \
+              f"&latitude={lat}&longitude={lon}&azimuth={azimuth}&tilt={tilt}&array_type=fixed" \
+              f"&hours={hours}&period=PT30M&format=json" \
+              f"&output_parameters=air_temp,gti,precipitation_rate,wind_speed_10m,wind_direction_10m"
+
+    data = requests.get(url=URL).json()
+    # Solcast formats the output as an object with a single field, "estimated-actuals", which contains a single array of all the data
+    return data["estimated_actuals"]
 
 
 def init_table():
@@ -47,6 +154,23 @@ def init_table():
         connection.close()
 
     return None
+
+
+def insert_dummy_data(days, days_prior):
+    # TODO docstring, ensure error message is right
+    # TODO use this https://docs.python.org/3/library/datetime.html#datetime-objects
+    # TODO and datetime.datetime.now().isoformat()
+
+    # try to create dt timezone info using
+    # import datetime, import pytz
+    # dt = dt.replace(tzinfo=pytz.FixedOffset(-h * 60))
+    # print(dt.utcoffset().seconds//3600 - 24)
+    # note the latter line will probably break in places other than the states
+
+    if days_prior <= days:
+        raise Exception("Error: When using insert_dummy_data, please ensure days_prior is at least days + 1 to ensure enough historical data exists in the Solcast historical database.")
+    startTime = datetime.datetime.now() + datetime.timedelta(days=-days_prior)
+    pass
 
 
 def insert_data(csv_filepath):
@@ -99,6 +223,12 @@ def insert_data(csv_filepath):
 
     return None
 
+
 if __name__ == "__main__":
-    init_table()
+    # init_table()
     # insert_data()
+    # TODO UNCOMMENT THE ABOVE
+
+    load_dotenv()
+
+    print(get_irradiance(os.getenv("SOLCAST_API_KEY"),43.464049, -80.559942, 0, 0, 5, True))
