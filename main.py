10e1ff5e009789de076bde858356ab0605ee6adb
kostubhagarwal <95394236+kostubhagarwal@users.noreply.github.com>
2025-02-23 05:01:26 -0500

    improved simulator (#73)
    * implemented new gradient_resistance func, battery cap constraint +  refactor
    
    * jakeread
    
    * autograd
    
    * irradiance_map
    
    * irradiance_map, discretization, and more
    
    * reduced memory usage in map_irradiance
    
    * Stop tracking autograd.ipynb
    
    * reduced memory usage in map_irradiance
    
    ---------
    
    Co-authored-by: root <root@tubbypc>

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from simulation import sim
 
 def main():
     # Simulation parameters
-    DISC = 3600  # Time intervals (seconds)
-    INTER = 8  # Total time (hours)
+    DISC = 3600*8  # Time intervals (seconds)
     STAGE_SYMBOL = "1B" # Stage
     CURRENT_D = 0e3 # Starting distance (m)
-    velocities = np.random.uniform(10, 20, 3600) # Sample velocity profile
+
+    # Simulation input
+    velocities = np.random.uniform(10, 20, DISC) # Sample velocity profile (m/s)
 
     # Run simulation
-    final_capacity, sim_data = sim(velocities, DISC, INTER, STAGE_SYMBOL, CURRENT_D)
+    final_capacity, sim_data = sim(velocities, DISC, STAGE_SYMBOL, CURRENT_D)
+    plt.plot(sim_data[:, 4]) # capacity graph (Wh), can power profiles (W) too
     print(final_capacity)
-
-    ## Run optimization (THIS DOESN"T WORK RN)
-    # optimized_velocities, final_capacity = optimize_velocity(velocities, DISC, INTER, STAGE_SYMBOL, CURRENT_D)
 
 if __name__ == "__main__":
     main()
