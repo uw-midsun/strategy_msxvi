55c5e893ffc470465426760c976eea3518efe297
Song Eric Yu Li <78510360+ericli3690@users.noreply.github.com>
2025-07-20 15:46:38 -0400

    Update telemetry for Windows, clean up simulation, first try at optimize (#77)
    * minor edits for connecting to telemetry via Windows
    
    - Minor changes to telemetry/main.py to remove WSL-only code, two new files in gitignore because the simplest, dirtiest solution to running telemetry on Windows is installing pip directly into the repository (yeah, it's gross), and a documentation file so no one has to experience my pain again
    
    * Clean up simulation, first try at optimize
    
    - Took a shot at implementing Nelder-Mead for optimization so that running main.py actually outputs something slightly interesting that other people can build off of
    - Did a bit of cleanup of code in main, overview, optimizer, and simulation, but lots of things are still messy
    - Fixed bugs in the simulator

--- overview.py
+++ overview.py
@@ -1,30 +1,44 @@
 import matplotlib.pyplot as plt
 from db import load_data_to_memory
 
-# Stage
-STAGE_SYMBOL = '1B'
+def show_overview(STAGE_SYMBOL):
+    # Load data
+    route_model_df, _ = load_data_to_memory()
+    route_model_df = route_model_df.sort_values(by="distance")
 
-# Load data
-route_model_df, irradiance_df = load_data_to_memory()
-route_model_df = route_model_df.sort_values(by="distance")
+    # Stage distance
+    stage_d = get_stage_dist(route_model_df, STAGE_SYMBOL)
+    stage_d_in_km = stage_d / 1000
+    print(f'Stage {STAGE_SYMBOL} total distance: {stage_d_in_km:.2f} km')
 
-# Stage distance
-stage_d = (route_model_df.loc[route_model_df['stage_name'].str.startswith(f'{STAGE_SYMBOL}_'), 'distance'].iloc[-1] - 
-           route_model_df.loc[route_model_df['stage_name'].str.startswith(f'{STAGE_SYMBOL}_'), 'distance'].iloc[0]) / 1000
-print(f'Stage {STAGE_SYMBOL} total distance: {stage_d:.2f} km')
+    # Stage elevation profile
+    plt.figure(figsize=(14, 7))
+    plt.plot(route_model_df.loc[route_model_df['stage_name'].str.startswith(f'{STAGE_SYMBOL}_')]['distance'], route_model_df.loc[route_model_df['stage_name'].str.startswith(f'{STAGE_SYMBOL}_')]['elevation'])
+    plt.title(f'{STAGE_SYMBOL} Elevation Profile')
+    plt.xlabel('Distance (m)')
+    plt.ylabel('Elevation (m)')
 
-# Stage elevation profile
-plt.figure(figsize=(14, 7))
-plt.plot(route_model_df.loc[route_model_df['stage_name'].str.startswith(f'{STAGE_SYMBOL}_')]['distance'], route_model_df.loc[route_model_df['stage_name'].str.startswith(f'{STAGE_SYMBOL}_')]['elevation'])
-plt.title(f'{STAGE_SYMBOL} Elevation Profile')
-plt.xlabel('Distance (m)')
-plt.ylabel('Elevation (m)')
+    plt.show()
 
-# Total elevation profile
-plt.figure(figsize=(14, 7))
-for symbol, subset in route_model_df.groupby('stage_name'):
-    plt.plot(subset['distance'], subset['elevation'], label=symbol)
-plt.title('Total Elevation Profile')
-plt.xlabel('Distance (m)')
-plt.ylabel('Elevation (m)')
-plt.legend()
+    # Total elevation profile
+    plt.figure(figsize=(14, 7))
+    for symbol, subset in route_model_df.groupby('stage_name'):
+        plt.plot(subset['distance'], subset['elevation'], label=symbol)
+    plt.title('Total Elevation Profile')
+    plt.xlabel('Distance (m)')
+    plt.ylabel('Elevation (m)')
+    plt.legend()
+
+    plt.show()
+
+def get_stage_dist(sorted_route_model_df, STAGE_SYMBOL):
+    return get_stage_end_dist(sorted_route_model_df, STAGE_SYMBOL) - get_stage_start_dist(sorted_route_model_df, STAGE_SYMBOL)
+
+def get_stage_start_dist(sorted_route_model_df, STAGE_SYMBOL):
+    return sorted_route_model_df.loc[sorted_route_model_df['stage_name'].str.startswith(f'{STAGE_SYMBOL}_'), 'distance'].iloc[0]
+
+def get_stage_end_dist(sorted_route_model_df, STAGE_SYMBOL):
+    return sorted_route_model_df.loc[sorted_route_model_df['stage_name'].str.startswith(f'{STAGE_SYMBOL}_'), 'distance'].iloc[-1]
+
+if __name__ == "__main__":
+    show_overview('1B')
